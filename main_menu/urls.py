335563f9506e62fb3b765d0bf1fedede7afe7cd8
Yuriy Khalak <khalak.yuriy@gmail.com>
2023-03-19 19:40:08 -0300

    error message on wrong credentials

--- main_menu/urls.py
+++ main_menu/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    #path('', views.login_view, name='m_login'),
+    #path(r'^(?:(?P<login_err>\d+)/)?/$', views.index, name='index'),
+    path('m_login', views.login_view, name='m_login'),
     #path('', views.logout_view, name='m_logout'),
 ]
--- main_menu/views.py
+++ main_menu/views.py
@@ -1,28 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
-
 from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def index(request):
     template = loader.get_template('menu.html')
     context = {
+        "login_err": request.GET.get('login_err', None)
     }
     return HttpResponse(template.render(context, request))
 
 
 def login_view(request):
-    username = request.POST['username']
-    password = request.POST['password']
+    if request.user.is_authenticated:
+         redirect('/menu')
+    username = request.POST.get('username')
+    password = request.POST.get('password')
     user = authenticate(request, username=username, password=password)
     if user is not None:
         login(request, user)
         # Redirect to a success page.
-        return redirect(view_to_redirect_to)
+        return redirect('/menu')
     else:
         # Return an 'invalid login' error message.
-        return redirect(view_to_redirect_to)
+        response = redirect('/menu')
+        response['Location'] += '?login_err=1'
+        return response
 
 def logout_view(request):
     logout(request)
