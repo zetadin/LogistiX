86b98ecf2311c936533e751e69bccf19cb735664
Yuriy Khalak <khalak.yuriy@gmail.com>
2023-05-28 15:23:49 -0300

    map selection via GET parameter

--- warroom/views.py
+++ warroom/views.py
@@ -3,10 +3,19 @@ from django.http import HttpResponse
 from django.template import loader
 from django.core.exceptions import PermissionDenied
 
+from warroom.map.models import Map, Hex
+
 # Create your views here.
 
 def warroom(request):
     if(request.user.is_authenticated and request.user.is_active):
+
+        mapid = request.GET.get('mapid', "")
+        if(mapid):
+            map_query = Map.objects.filter(name=mapid)
+            if(len(map_query)!=1):
+                #either no matches or multiple matches
+                return(redirect('/warroom', mapid=""))
         context = {}
         template = loader.get_template('warroom.html')
         return HttpResponse(template.render(context, request))
@@ -19,6 +28,12 @@ def mapeditor(request):
         if(not request.user.is_staff):
             # only acessible to admins
             raise PermissionDenied
+        mapid = request.GET.get('mapid', "")
+        if(mapid):
+            map_query = Map.objects.filter(name=mapid)
+            if(len(map_query)!=1):
+                #either no matches or multiple matches
+                return(HttpResponse("Map "+mapid+" does not exist. Create it via admin first."))
         context = {}
         template = loader.get_template('mapeditor.html')
         return HttpResponse(template.render(context, request))
